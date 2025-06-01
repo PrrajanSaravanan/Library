6c860b0992c9c7ad6feb5ba1346e76f30d37ae09
lokith <lokithk93@gmail.com>
2025-06-01 23:33:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const collection = require("./config");
 
 const app=express();
-const port =3000;
+const port =3000; 
 
 app.use(express.json());
 app.use(cors()); 
