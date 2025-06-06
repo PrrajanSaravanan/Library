e9ee97b0d8711ec0b584eef8fc947c300cf5d19b
lokith <lokithk93@gmail.com>
2025-06-06 10:14:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path=require('path');
 const body_parse=require('body-parser');
-const cors=require("cors");
+const cors=require("cors"); 
 const bcrypt = require('bcrypt');  
 const collection = require("./config");
 
